b7e6774eb7e3f9fb1693327dd66b3b10edc6d8f0
m <mirage060806@naver.com>
2026-09-14 17:17:54 +0900

    0914

---
+++ src/main/java/com/onrender/homepick/th/Ex07_Controller.java
@@ -0,0 +1,26 @@
+package com.onrender.homepick.th;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class Ex07_Controller {
+
+    @GetMapping("/th/ex07")
+    public String post(Model model) {
+        model.addAttribute("postId", 1); // 기본 1번으로 지정
+        return "th/ex07";
+    }
+
+    // 경로 변수 매핑 (/th/ex06/1, /th/ex07/2 ...)
+    @GetMapping("/th/ex07/{id}")
+    public String postDetail(@PathVariable("id") int id, Model model) {
+
+        // 넘겨받은 글 번호를 화면으로 전달
+        model.addAttribute("postId", id);
+
+        return "th/ex07"; // templates/th/ex07.html 반환
+    }
+}
