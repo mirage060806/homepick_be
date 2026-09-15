47a3727ef96f6c625e8405a73d5c78e1612e2c09
m <mirage060806@naver.com>
2026-09-15 15:56:09 +0900

    0915

--- src/main/java/com/onrender/homepick/controller/HomeController.java
+++ src/main/java/com/onrender/homepick/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.onrender.homepick.controller;
 
+import com.onrender.homepick.dto.MemberSessionDto;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController{
 
     @GetMapping("/")
-    public String index(){
-        return "index"; // templates/index.html 매핑
+    public String index(HttpSession session, Model model){
+        MemberSessionDto user = (MemberSessionDto) session.getAttribute("loginUser");
+        model.addAttribute("user", user);
+        return "index"; // templates/index.html
     }
 }
---
+++ src/main/java/com/onrender/homepick/dto/MemberSessionDto.java
@@ -0,0 +1,11 @@
+package com.onrender.homepick.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberSessionDto{
+    private String email;
+    private String name;
+}
